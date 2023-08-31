ea2f4c74c76f0a3427978eab598ce57d2bbfc9f1
Vladislav Danilov <Vladislav_Danilov@rntgroup.com>
2023-08-31 13:43:43 +0400

    Starting implementing images

--- src/app/pages/games/elements/game-list-item/game-list-item.component.ts
+++ src/app/pages/games/elements/game-list-item/game-list-item.component.ts
@@ -1,7 +1,9 @@
-import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from '@angular/core';
+import {ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 import {IGame} from '../../../../common/types/IGame';
 import {EStatus} from '../../../../common/types/EStatus';
+import {GamesService} from '../../games.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'game-list-item',
@@ -9,8 +11,9 @@ import {EStatus} from '../../../../common/types/EStatus';
   styleUrls: ['./game-list-item.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class GameListItemComponent {
+export class GameListItemComponent implements OnInit {
 
+  private subscription: Subscription;
   @Input()
   public game: IGame = {
     console: '',
@@ -22,8 +25,16 @@ export class GameListItemComponent {
 
   @Output()
   public editGameRequest = new EventEmitter<number>();
+  public imageUrl: string = '';
 
-  constructor() {
+  constructor(private gameService: GamesService) {
+    this.subscription = gameService.imageLoadChannel.subscribe((url: string) => {
+      this.imageUrl = url;
+    })
+  }
+
+  ngOnInit() {
+    this.gameService.imageLoadChannel.next(`${this.game.name} ${this.game.console}`)
   }
 
   onEditGame(gameID: number) {
--- src/app/pages/games/games.service.ts
+++ src/app/pages/games/games.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {SocialAuthService} from 'angularx-social-login';
 import {iif, of, throwError} from 'rxjs';
 import {catchError, map, mergeMap, switchMap} from 'rxjs/operators';
+import scraper from 'images-scraper';
 
 import {ErrorService} from '../../parts/error/error.service';
 import {LocalStorageService} from '../../common/services/local-storage.service';
@@ -12,11 +13,13 @@ import {Channel} from '../../../../MyTools/channel-conception/Channel';
 import {getFilteredGames} from './logics/getFilteredGames';
 import {combineGamesByStatus} from './logics/combineGamesByStatus';
 import {ICombinedGamesObject} from '../../common/types/ICombinedGamesObject';
+import {fromPromise} from 'rxjs/internal-compatibility';
 
 @Injectable()
 export class GamesService {
 
   public gamesLoadChannel: Channel<Filter | null, ICombinedGamesObject>;
+  public imageLoadChannel: Channel<string, string>;
 
   constructor(
     private socialAuthService: SocialAuthService,
@@ -35,6 +38,12 @@ export class GamesService {
       map((backup: IBackup) => combineGamesByStatus(backup.games))
     )
 
+    this.imageLoadChannel = new Channel((search) => fromPromise(this.getImage(search)).pipe(
+      map((value) => {
+        return value[0].url;
+      })
+    ))
+
     this.gamesLoadChannel = new Channel<Filter | null, ICombinedGamesObject>(
       (filter: Filter | null) => of(filter).pipe(
         mergeMap(() =>
@@ -50,5 +59,13 @@ export class GamesService {
         })
       )
     )
+  }
+
+  async getImage(search: string) {
+    const google = new scraper({
+      puppeteer: {
+      },
+    });
+    return await google.scrape(search, 1);
   }
 }
